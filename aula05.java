cd6ce724cba8f19e22bca9aa49d79bb24972f621
JoaoVitorjacques <vitorjoao60951@gmail.com>
2026-03-03 23:56:00 +0000

    criar vetor com 100 posições

---
+++ aula05.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+
+public class aula05 {
+
+    public static Random random = new Random();
+    public static void main(String[] args) {
+
+        int[] vetor = new int[100];
+        int soma = 0;
+
+        for (int i = 0; i < vetor.length; i++) {
+            vetor[i] = random.nextInt(30) + 1; // 1 a 30
+            soma += vetor[i];
+        }
+
+        double media = (double) soma / vetor.length;
+
+        System.out.print("{");
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.print(vetor[i]);
+            if (i < vetor.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("}");
+
+        System.out.println("Média dos valores: " + media);
+    }
+}
